b33c709d3f5a77ff8f578463351817d57f113f18
GaoJ <gao@666.com>
2019-12-18 22:12:28 +0800

    黑马头条PC-登录模块-数据绑定及校验

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,13 +1,88 @@
 <template>
-  <div>登录页面</div>
+  <!-- <div class="login"><span>登录页面</span></div> -->
+  <div class="login">
+    <el-card class="login-card">
+      <div class="title">
+        <img src="../../assets/img/logo_index.png" alt />
+      </div>
+      <el-form :model="loginForm" :rules="loginRules" ref="myForm">
+        <el-form-item prop="mobile">
+          <el-input v-model="loginForm.mobile" style="margin-top:28px" placeholder="请输入手机号"></el-input>
+        </el-form-item>
+        <el-form-item prop="code">
+          <el-input v-model="loginForm.code" style="width:66%" placeholder="请输入验证码"></el-input>
+          <el-button style="float:right" plain>发送验证码</el-button>
+        </el-form-item>
+        <el-form-item prop="check">
+            <el-checkbox v-model="loginForm.check">我一已同意用户协议和隐私条款</el-checkbox>
+        </el-form-item>
+        <el-form-item>
+            <el-button @click="submitLogin" type="primary" style="width:100%">登录</el-button>
+        </el-form-item>
+      </el-form>
+    </el-card>
+  </div>
 </template>
 
 <script>
 export default {
-
+  data () {
+    return { loginForm: {
+      mobile: '',
+      code: '',
+      check: 'false'
+    },
+    loginRules: {
+      mobile: [{ required: true, message: '请输入手机号' }, { pattern: /^1[3456789]\d{9}$/, message: '请输入合法的手机号' }],
+      code: [{ required: true, message: '请输入验证码' }, { pattern: /^\d{6}$/, message: '验证码为6位数字' }],
+      check: [{ validator: function (rule, value, callback) {
+        if (value) {
+          callback()
+        } else {
+          callback(new Error('请先阅读协议！'))
+        }
+      } }]
+    }
+    }
+  },
+  methods: {
+    submitLogin () {
+      this.$refs.myForm.validate(function (isOk) {
+        if (isOk) {
+          // 调用借口
+          alert('调用借口')
+        }
+      })
+    }
+  }
 }
 </script>
-
-<style>
-
+<style lang="less" scoped>
+.login {
+  background-image: url("../../assets/img/login_bg.jpg");
+  height: 100vh;
+  background-size: cover;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  .login-card {
+    width: 460px;
+    height: 340px;
+    .title {
+      text-align: center;
+      img {
+        //  margin-top: 30px;
+        height: 45px;
+      }
+    }
+  }
+}
 </style>
+
+<!-- <style lang="less" scoped>
+// .login {
+//     span {
+//         color: #f00;
+//         }
+// }
+// </style> -->
