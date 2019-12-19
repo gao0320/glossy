369e6532ca9d4f444986bce4e9315a39f6738fb4
GaoJ <gao@666.com>
2019-12-19 20:15:36 +0800

    黑马头条PC-主页模块-左侧导航菜单

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,6 +1,8 @@
 <template>
  <el-container>
-   <el-aside>左边</el-aside>
+   <el-aside style="background-color:#353b4e;height:100vh;width:230px">
+     <layout-aside></layout-aside>
+   </el-aside>
    <el-container>
       <el-header>头部</el-header>
       <el-main>主要区域</el-main>
@@ -10,8 +12,11 @@
 </template>
 
 <script>
+import layoutAside from '../../components/home/layout-aside'
 export default {
-
+  components: {
+    'layout-aside': layoutAside
+  }
 }
 </script>
 
