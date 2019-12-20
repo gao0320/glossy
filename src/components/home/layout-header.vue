9f33e4c3bd4a8a01466d86280f63d76a6135cde7
GaoJ <gao@666.com>
2019-12-20 16:43:06 +0800

    头部用户信息及下拉列表

---
+++ src/components/home/layout-header.vue
@@ -0,0 +1,43 @@
+<template>
+  <el-row class="layout-header" type="flex" justify="space-between" align="middle">
+    <el-col class="left" :span="7">
+      <i class="el-icon-s-fold"></i>
+      <span class="title">江苏传智播客教育科技股份有限公司</span>
+    </el-col>
+    <el-col :span="4" class="right">
+      <el-row type="flex" justify="end" align="middle">
+        <img src="../../assets/img/325608.jpg" alt style="height:40px;width:40px;border-radius:50%" />
+        <el-dropdown>
+          <span>紫宫初雪</span>
+          <el-dropdown-menu slot="dropdown">
+            <el-dropdown-item>个人信息</el-dropdown-item>
+            <el-dropdown-item>Git地址</el-dropdown-item>
+            <el-dropdown-item>退出</el-dropdown-item>
+          </el-dropdown-menu>
+        </el-dropdown>
+      </el-row>
+    </el-col>
+  </el-row>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="less" scoped>
+.layout-header {
+  height: 50px;
+  .left {
+    font-size: 16px;
+    .title {
+      margin-left: 4px;
+      font-size: 14px;
+    }
+  }
+  .right{
+      img{
+          margin-right: 5px;
+      }
+  }
+}
+</style>
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -4,7 +4,9 @@
      <layout-aside></layout-aside>
    </el-aside>
    <el-container>
-      <el-header>头部</el-header>
+      <el-header>
+        <layout-header></layout-header>
+      </el-header>
       <el-main>主要区域</el-main>
    </el-container>
 
@@ -13,9 +15,12 @@
 
 <script>
 import layoutAside from '../../components/home/layout-aside'
+import layoutHeader from '../../components/home/layout-header'
 export default {
   components: {
-    'layout-aside': layoutAside
+    'layout-aside': layoutAside,
+    'layout-header': layoutHeader
+
   }
 }
 </script>
