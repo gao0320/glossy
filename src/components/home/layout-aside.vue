f8a3ad10bba04b241a93572c5adf84f70d0e5296
GaoJ <gao@666.com>
2019-12-21 16:42:23 +0800

    左侧导航路由配置

--- src/components/home/layout-aside.vue
+++ src/components/home/layout-aside.vue
@@ -3,21 +3,22 @@
     <div class="title">
         <img src="../../assets/img/logo_admin.png" alt="">
     </div>
-    <el-menu style="width:231px" background-color="#353b4e" text-color="#adafb5" active-text-color="#ffd04b">
-    <el-menu-item>首页</el-menu-item>
-    <el-submenu>
+     <!-- :router="true" 全写  开启路由模式 -->
+    <el-menu router style="width:231px" background-color="#353b4e" text-color="#adafb5" active-text-color="#ffd04b">
+    <el-menu-item index="/">首页</el-menu-item>
+    <el-submenu index="1">
         <template slot="title">内容管理</template>
-        <el-menu-item>发布文章</el-menu-item>
-        <el-menu-item>内容列表</el-menu-item>
-        <el-menu-item>评论列表</el-menu-item>
-        <el-menu-item>素材管理</el-menu-item>
+        <el-menu-item index="/home/publish">发布文章</el-menu-item>
+        <el-menu-item index="/home/articles">内容列表</el-menu-item>
+        <el-menu-item index="/home/comment">评论列表</el-menu-item>
+        <el-menu-item index="/home/material">素材管理</el-menu-item>
     </el-submenu>
-<el-submenu>
+<el-submenu index="2">
     <template slot="title">粉丝管理</template>
-        <el-menu-item>图文数据</el-menu-item>
-        <el-menu-item>粉丝面向</el-menu-item>
-        <el-menu-item>粉丝概况</el-menu-item>
-        <el-menu-item>粉丝现状</el-menu-item>
+        <el-menu-item index="/home/picture">图文数据</el-menu-item>
+        <el-menu-item index="/home/fansinfo">粉丝面向</el-menu-item>
+        <el-menu-item index="/home/fanslife">粉丝概况</el-menu-item>
+        <el-menu-item index="/home/fanslist">粉丝现状</el-menu-item>
 </el-submenu>
     <el-menu-item>账户信息</el-menu-item>
 </el-menu>
