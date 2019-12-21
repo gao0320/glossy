2e4c5440933137f6ec26f53989c104c9efe90a20
GaoJ <gao@666.com>
2019-12-21 17:19:03 +0800

    主页模块默认模块二级路由

--- src/router/index.js
+++ src/router/index.js
@@ -2,15 +2,21 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/home'
 import Login from '../views/login'
+import Home2 from '../views/home/home'
 Vue.use(VueRouter)
 const routes = [{
   path: '/',
   redirect: '/home'
 },
 {
+  // 主页
   path: '/home',
   name: 'home',
-  component: Home
+  component: Home,
+  children: [{
+    path: '',
+    component: Home2
+  }]
 }, {
   path: '/login',
   component: Login
