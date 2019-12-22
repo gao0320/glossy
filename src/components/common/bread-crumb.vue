f1880b1be9781bdf9d70d485d5df5f6a1843d580
GaoJ <gao@666.com>
2019-12-22 17:44:21 +0800

    注册面包屑组件与封装

---
+++ src/components/common/bread-crumb.vue
@@ -0,0 +1,20 @@
+<template>
+  <el-bread-crumb>
+      <el-breadcrumb-item to="/home">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>
+          <slot name="title">
+
+          </slot>
+      </el-breadcrumb-item>
+  </el-bread-crumb>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- src/components/index.js
+++ src/components/index.js
@@ -1,9 +1,11 @@
 // Vue.use全局注册组件
 import LayoutAside from './home/layout-aside'
 import LayoutHeader from './home/layout-header'
+import BreadCrumb from './common/bread-crumb'
 export default {
   install (Vue) {
     Vue.component('layout-aside', LayoutAside)
     Vue.component('layout-header', LayoutHeader)
+    Vue.component('bread-crumb', BreadCrumb)
   }
 }
