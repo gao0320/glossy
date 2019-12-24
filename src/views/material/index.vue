9fffe18cd76fd8691af7073a618c6395f52fad8c
GaoJ <gao@666.com>
2019-12-24 21:54:27 +0800

    黑马头条PC-素材列表-分页组件及请求

--- src/views/material/index.vue
+++ src/views/material/index.vue
@@ -16,18 +16,37 @@
                     </el-row>
                  </el-card>
               </div>
-          </el-tab-pane>
-          <el-tab-pane label="收藏素材" name="collect">
-              <!-- 收藏素材内容 -->
-              <div class="img-list">
-                <!-- v-for -->
-                 <el-card class='img-card' v-for="item in list" :key="item.id">
-                     <img :src="item.url" alt="">
-                 </el-card>
-              </div>
-          </el-tab-pane>
-
-      </el-tabs>
+          <el-row type="flex" justify="center" style="height:80px" align="middle">
+          <el-pagination
+            background
+            layout="prev, pager, next"
+            :total="page.total"
+            :current-page="page.currentPage"
+            :page-size="page.pageSize"
+            @current-change="changePage"
+          ></el-pagination>
+        </el-row>
+      </el-tab-pane>
+      <el-tab-pane label="收藏素材" name="collect">
+        <!-- 收藏素材内容 -->
+        <div class="img-list">
+          <!-- v-for -->
+          <el-card class="img-card" v-for="item in list" :key="item.id">
+            <img :src="item.url" alt />
+          </el-card>
+        </div>
+        <el-row type="flex" justify="center" style="height:80px" align="middle">
+          <el-pagination
+            background
+            layout="prev, pager, next"
+            :total="page.total"
+            :current-page="page.currentPage"
+            :page-size="page.pageSize"
+            @current-change="changePage"
+          ></el-pagination>
+        </el-row>
+      </el-tab-pane>
+    </el-tabs>
   </el-card>
 </template>
 
@@ -37,11 +56,21 @@ export default {
     return {
     //   activeName: 'collect'
       activeName: 'all',
-      list: []
+      list: [],
+      page: {
+        currentPage: 1, // 当前页码
+        pageSize: 8, // 每页多少条
+        total: 0 // 总条数
+      }
     }
   },
   methods: {
+    changePage (newPage) {
+      this.page.currentPage = newPage // 得到最新页码
+      this.getAllMaterial()
+    },
     changeTab () {
+      this.page.currentPage = 1
       this.getAllMaterial()
     },
     //   获取所有素材/收藏
@@ -49,9 +78,12 @@ export default {
       // all 所有 collect  收藏  this.activeName === "collect"
       this.$axios({
         url: '/user/images',
-        params: { collect: this.activeName === 'collect' }
+        params: { collect: this.activeName === 'collect',
+          page: this.page.currentPage,
+          per_page: this.page.pageSize }
       }).then(result => {
         this.list = result.data.results
+        this.page.total = result.data.total_count
       })
     }
   },
@@ -62,27 +94,27 @@ export default {
 </script>
 
 <style lang='less' scoped>
-    .img-list {
-     display: flex;
-     flex-wrap: wrap;
-     .img-card {
-         width: 200px;
-         height:240px;
-         margin: 20px 50px;
-         position: relative;
-         img {
-             width: 100%;
-             height: 100%;
-         }
-         .operate {
-             width: 100%;
-             font-size:20px;
-             position: absolute;
-             bottom: 0;
-             left:0;
-             background-color: #f4f5f6;
-             height:30px;
-         }
-     }
- }
+   .img-list {
+  display: flex;
+  flex-wrap: wrap;
+  .img-card {
+    width: 200px;
+    height: 240px;
+    margin: 20px 50px;
+    position: relative;
+    img {
+      width: 100%;
+      height: 100%;
+    }
+    .operate {
+      width: 100%;
+      font-size: 20px;
+      position: absolute;
+      bottom: 0;
+      left: 0;
+      background-color: #f4f5f6;
+      height: 30px;
+    }
+  }
+}
 </style>
