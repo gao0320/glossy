db9e2172d812318c9ec924953b793d90739c62ab
GaoJ <gao@666.com>
2019-12-24 18:00:49 +0800

    头条PC-统一处理大数字类型

--- src/utils/request.js
+++ src/utils/request.js
@@ -2,6 +2,7 @@
 import axios from 'axios'
 import router from '../router'
 import { Message } from 'element-ui'
+import JSONBig from 'json-bigint'
 axios.defaults.baseURL = 'http://ttapi.research.itcast.cn/mp/v1_0'
 axios.interceptors.request.use(function (config) {
   // 在发起请求前做一些业务处理
@@ -9,10 +10,13 @@ axios.interceptors.request.use(function (config) {
   let token = window.localStorage.getItem('user-token') // 获取token
   config.headers.Authorization = `Bearer ${token}` // 统一注入token 到headers属性 因为所有接口要求的token格式是一样的
   return config
-}, function (errors) {
+}, function () {
   // 对请求失败做处理
-  return Promise.reject(errors)
+
 })
+axios.defaults.transformResponse = [function (data) {
+  return JSONBig.parse(data) // JSONbig.parse 替换 JSON.parse  保证数字的正确
+}]
 axios.interceptors.response.use(function (response) {
   // 成功
   return response.data ? response.data : {}// 解决data不存在时then中读取数字报错问题
