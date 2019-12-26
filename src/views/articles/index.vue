c84c8f303307d9f267f1b85afde3d86b3ab0305e
GaoJ <gao@666.com>
2019-12-26 23:17:02 +0800

    黑马头条PC-内容列表-分页请求

--- src/views/articles/index.vue
+++ src/views/articles/index.vue
@@ -44,7 +44,8 @@
     </el-row>
      <!-- 主体 -->
     <el-row class='total'>
-        <span>共找到1000条符合条件的内容</span>
+        <!-- <span>共找到100条符合条件的内容</span> -->
+        <span>共找到{{ page.total }}条符合条件的内容</span>
     </el-row>
     <!-- 循环的模板 -->
     <!-- <el-row  v-for="item in 100" :key="item" class='article-item' type='flex' justify="space-between"> -->
@@ -73,6 +74,18 @@
            </el-row>
        </el-col>
     </el-row>
+     <!-- 分页组件 -->
+    <el-row type='flex' justify="center" align="middle" style='height:60px'>
+      <el-pagination
+        background
+        layout="prev, pager, next"
+        :total="page.total"
+        :current-page="page.currentPage"
+        :page-size="page.pageSize"
+        @current-change="changePage"
+        >
+        </el-pagination>
+    </el-row>
   </el-card>
 </template>
 
@@ -87,7 +100,12 @@ export default {
       },
       channels: [], // 定义一个channels 接收频道
       list: [], // 接收文章列表数据
-      defaultImg: require('../../assets/img/default.png')
+      defaultImg: require('../../assets/img/default.png'),
+      page: {
+        currentPage: 1, // 当前页码
+        pageSize: 10, // 文章列表最低10条
+        total: 0
+      }
     }
   },
   filters: {
@@ -127,11 +145,23 @@ export default {
 
   },
   methods: {
+    // 改变页码事件
+    changePage (newPage) {
+      // 赋值当前页码
+      this.page.currentPage = newPage // 赋值当前页
+      this.getConditionArticle()
+    },
     // 改变条件
     changeCondition () {
       // 组装条件
+      this.page.currentPage = 1 // 强制将当前的页码回到第一页
       // 最新状态
+      this.getConditionArticle()
+    },
+    getConditionArticle () {
       let params = {
+        page: this.page.currentPage, // 分页信息
+        per_page: this.page.pageSize, // 分页信息
         status: this.formData.status === 5 ? null : this.formData.status, // 不传为全部 5代表全部
         channel_id: this.formData.channel_id, // 频道
         begin_pubdate: this.formData.dateRange.length ? this.formData.dateRange[0] : null, // 起始时间
@@ -148,18 +178,19 @@ export default {
       })
     }
   },
-  // 获取文章列表数据
+  // 获取文章列表数据 分页 切换/ 条件切换
   getArticles (params) {
     this.$axios({
       url: '/articles', // 请求地址
       params
     }).then(result => {
       this.list = result.data.results // 接收文章列表数据
+      this.page.total = result.data.total_count // 文章总数
     })
   },
   created () {
     this.getChannels() // 调用获取频道数据
-    this.getArticles() // 调用获取文章列表
+    this.getArticles({ page: 1, per_page: 10 }) // 调用获取文章列表
   }
 }
 </script>
