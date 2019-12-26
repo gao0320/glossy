4c915f5e3771d018172db1afc3dd8c613a748637
GaoJ <gao@666.com>
2019-12-26 22:42:24 +0800

    黑马头条PC-内容列表-页面结构-搜索工具栏

--- src/views/articles/index.vue
+++ src/views/articles/index.vue
@@ -1,17 +1,83 @@
 <template>
-  <el-card>
-      <bread-crumb slot="header">
-        <template slot='title'>文章列表</template>
-      </bread-crumb>
+  <el-card class="articles">
+    <bread-crumb slot="header">
+      <template slot="title">文章列表</template>
+    </bread-crumb>
+    <el-row class="searchTool">
+      <el-row :span="2">
+        <span>文章状态</span>
+      </el-row>
+      <el-row :span="18">
+        <el-radio-group v-model="formData.status">
+          <el-radio :label="5">全部</el-radio>
+          <el-radio :label="0">草稿</el-radio>
+          <el-radio :label="1">待审核</el-radio>
+          <el-radio :label="2">审核通过</el-radio>
+          <el-radio :label="3">审核失败</el-radio>
+        </el-radio-group>
+      </el-row>
+    </el-row>
+    <el-row class="searchTool">
+        <el-col :span="2">
+              <span>频道列表</span>
+        </el-col>
+        <el-col :span="18">
+            <el-select v-model="formData.channel_id">
+                <el-option vi-for="item in channels" :ket="item.id" :label="item.name"
+             :value="item.id"></el-option>
+            </el-select>
+        </el-col>
+    </el-row>
+    <el-row class="searchTool">
+        <el-col :span="2">
+            <span>时间选择</span>
+        </el-col>
+     <el-col :span="18">
+        <el-date-picker
+          v-model="formData.dateRange"
+          type="daterange"
+          range-separator="-"
+          start-placeholder="开始日期"
+          end-placeholder="结束日期"
+        ></el-date-picker>
+      </el-col>
+    </el-row>
   </el-card>
 </template>
 
 <script>
 export default {
-
+  data () {
+    return {
+      formData: {
+        status: 5, // 状态
+        channel_id: null, // 默认是空
+        dateRange: []
+      },
+      channels: [] // 定义一个channels 接收频道
+    }
+  },
+  methods: {
+    //   获取频道
+    getChannels () {
+      this.$axios({
+        url: '/channels'
+      }).then(result => {
+        this.channels = result.data.channels // 获取频道数据
+      })
+    }
+  },
+  created () {
+    this.getChannels() // 调用获取频道数据
+  }
 }
 </script>
 
-<style>
-
+<style lang='less' scoped>
+.articles {
+  .searchTool {
+    height: 60px;
+    padding-left: 50px;
+  }
+}
 </style>
